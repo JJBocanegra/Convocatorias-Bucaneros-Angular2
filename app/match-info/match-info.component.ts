daabd0552da7b9719f00ed32bcfbe7799eb85bf4
José Joaquín Bocanegra <bocanegra.velasco@gmail.com>
2016-03-10 01:03:10 +0100

    feat(match-info): now a player can be confirmed or injured

--- app/match-info/match-info.component.ts
+++ app/match-info/match-info.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from 'angular2/core';
 import {Match} from './match';
+import {Player} from '../player/player';
 import {MatchInfoService} from './match-info.service';
-import {PlayerService} from '../player/player.service'
+import {PlayerService} from '../player/player.service';
+import {HelperService} from '../helper/helper.service';
 
 @Component({
   selector: 'match-info',
@@ -9,13 +11,17 @@ import {PlayerService} from '../player/player.service'
   providers: [MatchInfoService, PlayerService],
 })
 export class MatchInfo implements OnInit {
-  public confirmedPlayers: Object[] = [];
-  public injuredPlayers: Object[] = [];
+  public confirmedPlayers: Player[] = [];
+  public injuredPlayers: Player[] = [];
   public matches: Match[] = [];
   public nextMatch: Match;
-  public notConfirmedPlayers: Object[] = [];
+  public notConfirmedPlayers: Player[] = [];
+  public selectedPlayer: number;
 
-  constructor(private matchInfoService: MatchInfoService, private playerService: PlayerService) { }
+  constructor(
+    private matchInfoService: MatchInfoService,
+    private playerService: PlayerService,
+    private helperService: HelperService) { }
 
   ngOnInit(): void {
     this.getNextMatch();
@@ -28,7 +34,7 @@ export class MatchInfo implements OnInit {
           this.nextMatch = response[0];
           this.getPlayers();
         },
-        error => console.log(error)
+        error => {}
       );
   }
 
@@ -42,10 +48,10 @@ export class MatchInfo implements OnInit {
     this.matchInfoService.getConfirmedPlayersByMatchId(this.nextMatch.matchId)
       .subscribe(
         response => {
-          var players = this.playerService.getFullNameWithoutLastSurnameOfPlayers(response);
+          var players = this.playerService.getPlayersFullNames(response);
           this.confirmedPlayers = players;
         },
-        error => console.log(error)
+        error => {}
       );
   }
 
@@ -53,10 +59,10 @@ export class MatchInfo implements OnInit {
     this.matchInfoService.getNotConfirmedPlayersByMatchId(this.nextMatch.matchId)
       .subscribe(
         response => {
-          var players = this.playerService.getFullNameWithoutLastSurnameOfPlayers(response);
+          var players = this.playerService.getPlayersFullNames(response);
           this.notConfirmedPlayers = players;
         },
-        error => console.log(error)
+        error => {}
       );
   }
 
@@ -64,10 +70,32 @@ export class MatchInfo implements OnInit {
     this.matchInfoService.getInjuredPlayersByMatchId(this.nextMatch.matchId)
       .subscribe(
         response => {
-          var players = this.playerService.getFullNameWithoutLastSurnameOfPlayers(response);
+          var players = this.playerService.getPlayersFullNames(response);
           this.injuredPlayers = players;
         },
-        error => console.log(error)
+        error => {}
       );
+  }
+
+  confirmPlayer(): void {
+    this.matchInfoService.confirmPlayer(this.nextMatch.matchId, this.selectedPlayer)
+    .subscribe(
+      player => {
+        this.getConfirmedPlayers();
+        this.getNotConfirmedPlayers();
+      },
+      error => {}
+    );
+  }
+
+  addInjuredPlayer(): void {
+    this.matchInfoService.addInjuredPlayer(this.nextMatch.matchId, this.selectedPlayer)
+    .subscribe(
+      player => {
+        this.getInjuredPlayers();
+        this.getNotConfirmedPlayers();
+      },
+      error => {}
+    );
   }
 }
