39ad40192e35ccef1be170d6768e0cbce27e1c52
José Joaquín Bocanegra <bocanegra.velasco@gmail.com>
2016-03-10 01:01:15 +0100

    feat(helper): create method and improve error msg
    create the method *indexOfObject* and now it shows only the body of the
    error

--- app/helper/helper.service.ts
+++ app/helper/helper.service.ts
@@ -9,7 +9,15 @@ export class HelperService {
   handleError(error: Response) {
     // in a real world app, we may send the error to some remote logging infrastructure
     // instead of just logging it to the console
-    console.error(error);
-    return Observable.throw(error.json().error || 'Server error');
+    console.error(error.text());
+    return Observable.throw(error.json().error || error.text());
+  }
+
+  indexOfObject(array: any[], key: any, value: any): any {
+    for (var i = array.length - 1; i >= 0; i--) {
+      if (array[i][key] == value) {
+        return i;
+      }
+    }
   }
 }
