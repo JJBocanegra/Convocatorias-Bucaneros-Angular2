e57eaf58dc5f83cd128b58faa3235790d94097b4
José Joaquín Bocanegra <bocanegra.velasco@gmail.com>
2016-03-12 22:50:04 +0100

    feat: add angular routing to the app

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,17 @@
 import {Component} from 'angular2/core';
-import {OnInit} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {MatchInfo} from './match-info/match-info.component';
+import {PlayerInfo} from './player/player.component';
+import {NewPlayer} from './player/new-player.component';
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.html',
-  directives: [MatchInfo],
+  directives: [MatchInfo, NewPlayer, ROUTER_DIRECTIVES],
 })
-export class AppComponent implements OnInit {
-  ngOnInit() {};
-}
+@RouteConfig([
+  {path:'/convocatoria', name: 'MatchInfo', component: MatchInfo},
+  {path:'/jugador/:id', name: 'PlayerInfo', component: PlayerInfo},
+  {path:'/nuevo-jugador', name: 'NewPlayer', component: NewPlayer},
+])
+export class AppComponent {}
--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,16 @@
 import {bootstrap} from 'angular2/platform/browser'
 import {AppComponent} from './app.component'
 import {HTTP_PROVIDERS} from 'angular2/http';
+import {ROUTER_PROVIDERS} from 'angular2/router';
 
 import {DateTimeService} from './date-time/date-time.service';
 import {HelperService} from './helper/helper.service';
 
 import 'rxjs/Rx';
 
-bootstrap(AppComponent, [HTTP_PROVIDERS, DateTimeService, HelperService]);
+bootstrap(AppComponent, [
+  HTTP_PROVIDERS,
+  ROUTER_PROVIDERS,
+  DateTimeService,
+  HelperService
+]);
