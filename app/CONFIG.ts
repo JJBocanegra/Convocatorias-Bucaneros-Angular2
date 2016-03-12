7aeb4042f3faa35f4dfceed9c145b79f0d7772ab
José Joaquín Bocanegra <bocanegra.velasco@gmail.com>
2016-03-12 22:51:48 +0100

    feat(CONFIG): create a service to store global vars
    create the config service where will store all the variables that will
    be needed along the app, like the API url

---
+++ app/CONFIG.ts
@@ -0,0 +1,8 @@
+import {Injectable} from 'angular2/core';
+
+@Injectable()
+export class CONFIG {
+  public static apiUrl = 'http://localhost/Convocatorias-Bucaneros-Angular2/api/api.php';
+
+  constructor() { }
+}
