3ceb2e568d35df16126c4f5127afdd8fd48357e6
José Joaquín Bocanegra <bocanegra.velasco@gmail.com>
2016-03-13 22:53:51 +0100

    feat(api): create UpdatePlayer
    it also refactor the parameters of the statement, naming them to be more
    clear

--- api/classes/player.php
+++ api/classes/player.php
@@ -19,11 +19,40 @@ class Player {
       $sentence = "
         SELECT *
         FROM Player
-        WHERE playerId = ?";
+        WHERE playerId = :playerId";
 
-        $parameters = array($playerId);
+      $parameters = array(':playerId' => $playerId);
 
       return $this->DBUtils->QuerySelect($sentence, $parameters);
+    } catch (Exception $e) {
+      return "Error: ".$e->getMessage();
+    }
+  }
+
+  function UpdatePlayer($player, $playerId) {
+    try {
+      $sentence = "
+        Update Player
+        SET name = :name, firstSurname = :firstSurname, secondSurname = :secondSurname, nickname = :nickname, birthDate = :birthDate, hasImage = :hasImage
+        WHERE playerId = :playerId";
+
+      $playerData = json_decode($player, true);
+
+      $parameters = array(
+        ':playerId' => $playerId,
+        ':name' => $playerData['name'],
+        ':firstSurname' => $playerData['firstSurname'],
+        ':secondSurname' => $playerData['secondSurname'],
+        ':nickname' => $playerData['nickname'],
+        ':birthDate' => $playerData['birthDate'],
+        ':hasImage' => $playerData['hasImage'],
+      );
+
+      if ($this->DBUtils->Query($sentence, $parameters) === 1) {
+        return $this->GetPlayerById($playerId);
+      } else {
+        throw new Exception("The player $playerId couldn't be updated. The SQL sentence was $sentence");
+      }
     } catch (Exception $e) {
       return "Error: ".$e->getMessage();
     }
