bfe94c58f82aae40fcf927515f4f71dff33c8b4b
José Joaquín Bocanegra <bocanegra.velasco@gmail.com>
2016-03-15 23:27:36 +0100

    feat(new-player): create new-player component

---
+++ app/player/new-player.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from 'angular2/core';
+import {Player} from './player';
+import {PlayerService} from './player.service';
+
+@Component({
+  selector: 'new-player',
+  templateUrl: 'app/player/new-player.html',
+  providers: [PlayerService],
+})
+export class NewPlayer implements OnInit {
+  public player: Player = <Player> {};
+
+  constructor(
+    private playerService: PlayerService) { }
+
+  ngOnInit(): void {
+  }
+
+  createPlayer(): any {
+    this.playerService.createPlayer(this.player)
+      .subscribe(
+        player => {
+          this.player = <Player> {};
+          console.log("Jugador insertado con éxito");
+        },
+        error => { }
+      );
+  }
+}
