935c1958955c90861bacd9dd38857730c63059d9
JJ <bocanegra.velasco@gmail.com>
2016-09-15 23:09:20 +0200

    primeros pasos para adaptar la app a angular2 final

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {RouteConfig, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
+// import {RouteConfig, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
 
 import {CallComponent} from './call/index';
 import {MatchesComponent} from './matches/index';
@@ -8,13 +8,13 @@ import {PlayerInfo, NewPlayer} from './player/index';
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html',
-  directives: [CallComponent, NewPlayer, ROUTER_DIRECTIVES],
+  // directives: [CallComponent, NewPlayer, ROUTER_DIRECTIVES],
 })
-@RouteConfig([
-  {path: '/proxima-convocatoria', name: 'NextCall', component: CallComponent, useAsDefault: true},
-  {path: '/convocatoria', name: 'Calls', component: MatchesComponent},
-  {path: '/convocatoria/:id', name: 'Call', component: CallComponent},
-  {path: '/jugador/:id', name: 'PlayerInfo', component: PlayerInfo},
-  {path: '/nuevo-jugador', name: 'NewPlayer', component: NewPlayer},
-])
+// @RouteConfig([
+//   {path: '/proxima-convocatoria', name: 'NextCall', component: CallComponent, useAsDefault: true},
+//   {path: '/convocatoria', name: 'Calls', component: MatchesComponent},
+//   {path: '/convocatoria/:id', name: 'Call', component: CallComponent},
+//   {path: '/jugador/:id', name: 'PlayerInfo', component: PlayerInfo},
+//   {path: '/nuevo-jugador', name: 'NewPlayer', component: NewPlayer},
+// ])
 export class AppComponent {}
--- app/main.ts
+++ app/main.ts
@@ -1,15 +1,21 @@
-import {bootstrap} from '@angular/platform-browser-dynamic';
-import {AppComponent} from './app.component';
-import {HTTP_PROVIDERS} from '@angular/http';
-import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
-import 'rxjs/Rx';
+import {platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
 
-import {DateTimeService} from './date-time/index';
-import {HelperService} from './helper/index';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
 
-bootstrap(AppComponent, [
-  HTTP_PROVIDERS,
-  ROUTER_PROVIDERS,
-  DateTimeService,
-  HelperService,
-]);
+// import {bootstrap} from '@angular/platform-browser-dynamic';
+// import {AppComponent} from './app.component';
+// import {HTTP_PROVIDERS} from '@angular/http';
+// import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
+// import 'rxjs/Rx';
+//
+// import {DateTimeService} from './date-time/index';
+// import {HelperService} from './helper/index';
+//
+// bootstrap(AppComponent, [
+//   HTTP_PROVIDERS,
+//   ROUTER_PROVIDERS,
+//   DateTimeService,
+//   HelperService,
+// ]);
